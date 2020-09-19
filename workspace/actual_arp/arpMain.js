e99ce603af59ee22512cbee2bdc9f492baf4aea1
yhk94822 <70730823+yhk94822@users.noreply.github.com>
2020-09-20 00:15:17 +0900

    Add files via upload

--- workspace/actual_arp/arpMain.js
+++ workspace/actual_arp/arpMain.js
@@ -24,12 +24,12 @@ const playerName = 'Etc_Hoons';
 
 var myHeaders = new Headers();
 myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJhMjk1ZTQ2MC1kYmQ3LTAxMzgtMjViYS02OTdhNWQ2ZGY2YzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjAwNDMxNzg4LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImFycGNsYW4ifQ.cmVihwpRsp9D6bH22DEPrH2wT4j2regZHnGDDZChjtQ");
+myHeaders.append("Content-Type", "application/json");
 
 fetch('https://api.pubg.com/shards/'+platform+'/players?filter[playerNames]='+playerName ,{"headers":myHeaders})
 .then((res) => res.json())
 .then((data) => {
-    console.log(data);
-    //document.getElementById('battlegroundAPI').innerHTML = tag;
+    document.getElementById('battlegroundAPI').innerHTML = tag;
     
 
     //document.getElementById('apply_div').innerHTML = tag;
